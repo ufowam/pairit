addde1b6551ec10b1bfd735afc536dedfdc4cc51
ayrus <surya@hakc.net>
2012-12-01 18:33:49 -0500

    Integrated node/sockets. Chat feature for now.

--- app.js
+++ app.js
@@ -1,20 +1,74 @@
 // Including libraries
 
 var app = require('http').createServer(handler),
-	io = require('socket.io').listen(app),
-	static = require('node-static'),
-	md5h = require('MD5');
+    io = require('socket.io').listen(app),
+    static = require('node-static'),
+    md5h = require('MD5');
 
 var fileServer = new static.Server('./');
-	
+    
 app.listen(9000);
 
 function handler (request, response) {
 
-    request.addListener('end', function () {
-        fileServer.serve(request, response);
-    });
+var matchRoomRequest = /\/room\/(.{32})/;;
+
+    switch(true){
+
+        case request.url == '/create_session':
+
+            var roomID;
+
+            request.addListener('data', function(data){
+                roomID = md5h(new Date().getTime() + data.toString().split('=')[1]);
+            });
+
+            request.addListener('end', function () {
+                response.writeHead(302, {
+                    'Location' : 'room/' + roomID
+                });
+
+                response.end();
+                //fileServer.serveFile('/workspace.html', 200, {}, request, response);      
+            });
+
+            break;
+
+        case matchRoomRequest.test(request.url):
+
+            request.addListener('end', function () {
+                fileServer.serveFile('/workspace.html', 200, {}, request, response);        
+            });
+
+
+            break;
+
+        default:
+
+            request.addListener('end', function () {
+                fileServer.serve(request, response);
+            });
+
+            break;
+    }
+
 
 }
 
 io.set('log level', 3);
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('joinroom', function(data){
+
+        console.log("User joined:" + data.roomID);
+
+        socket.join(data.roomID);
+    });
+
+    socket.on('chatsend', function (data) {
+        
+        console.log("Chat rx: " + data.msg);
+        io.sockets.in(data.roomID).emit('chatreceive', data);
+    });
+});
--- assets/js/code.js
+++ assets/js/code.js
@@ -3,11 +3,61 @@ var language = "python";
 var editor;
 
 $(document).ready(function() {
-editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-  mode: {name: language},
-  lineNumbers: true,
-  indentUnit: 4,
-  tabMode: "shift",
-  theme: "monokai",
-  matchBrackets: true
-});});
+
+	setUpEditor();
+
+	comm();
+
+});
+
+function setUpEditor(){
+
+	editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+  	mode: {name: language},
+  	lineNumbers: true,
+  	indentUnit: 4,
+  	tabMode: "shift",
+  	theme: "monokai",
+  	matchBrackets: true
+	});
+}
+
+function comm(){
+
+	// The URL of your web server (the port is set in app.js)
+	//var url = 'http://localhost:9000';
+	var doc = $(document), win = $(window), textbox = $('#text');
+
+	var matchRoomRequest = /.*\/room\/(.{32})/;
+
+	var roomID = matchRoomRequest.exec(document.URL)[1];
+
+	alert(roomID);
+
+	var url = 'http://192.168.43.235:9000/';
+
+	var socket = io.connect(url);
+
+	//alert(matchRoomRequest.exec(document.URL)[1]);
+
+	socket.emit('joinroom', {
+		'roomID' : roomID
+	});
+
+	$('#chatsend').on('submit', function() {
+
+		socket.emit('chatsend', {
+			'msg': $('#msg').val(),
+			'roomID' : roomID
+		});
+
+		$("#msg").val("");
+
+		return false;
+	});
+
+	socket.on('chatreceive', function(data){
+		//alert(data.text);
+		textbox.val(data.text);
+	});
+}

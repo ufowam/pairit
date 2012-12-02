c5b7da5793454f16f54381cd1e78e47e954b2afb
Warren Marivel <warren@warren-laptop.(none)>
2012-12-02 05:55:26 -0500

    implemented gist upload feature

--- assets/js/download_file.js
+++ assets/js/download_file.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
-	$('#download_raw').click(function (){
+	$('#download_gist').submit(function (){
 
-		alert('elo');
-		var code = $('#code').html();
-		socket.emit('downloadcode', {
-				'name' : username,
-				'code': code
-			});
+		//alert("ok");
+		//alert($('#filename').val());
+		//alert($('#description').val());
+		var code = editor.getValue();
+		var filename = $('#filename').val();
+		var description = $('#description').val();
+		//alert(filename);
+		//alert(description);
+		var json = {
+  			"description": description,
+  			"public": true,
+  			"files": {
+    			filename: {
+      				"content": code
+    			}
+  			}
+		};
+
+		var jsontext = JSON.stringify(json);
+		jQuery.post("https://api.github.com/gists", jsontext, function(data, textStatus, jqXHR){
+			var jsonobject = JSON.parse(data);
+			var git_url = jsonobject.html_url;
+			$('#upload_gist').modal('hide');
+			$('#gist_url_div').modal('show');
+			$('#gist_url').html(git_url);
+		});
+		return false;
 	});
 });
